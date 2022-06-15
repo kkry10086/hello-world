a4808412f6fbabcd811a7861d99e5f1814f024f5
kkry10086 <19868558211@139.com>
2022-06-15 16:59:24 +0800

    change c++

---
+++ c++/chapter8-template fun/copy.h
@@ -0,0 +1,12 @@
+#GUAID_copy_h
+#define_find_h
+
+template <class In,class Out>
+Out copy(In begin,In end,Out desk){
+	while(begin!=end)
+		*dest++=*begin++;
+	return dest
+}
+
+
+#endif
---
+++ c++/chapter8-template fun/find.h
@@ -0,0 +1,19 @@
+#GUAID_find_h
+#define_find_h
+template<class In,class X>
+In find(In begin,In end,const X& x){
+    while(begin!=end && *begin !=x)
+	    ++begin;
+    return begin;
+}
+
+template<class In,class X> In find(In begin,In end,const X& x){
+
+	if(begin==end || *begin==x)
+		return begin;
+	++begin;
+	return find(begin,end,x)
+}
+
+
+#endif
